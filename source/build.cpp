449d5c561092e02dbfb9017a57f44aa7f668e824
Alexander Shafranov <shafranov@gmail.com>
2014-03-19 18:52:12 +0100

    initial build fixes.

--- source/build.cpp
+++ source/build.cpp
@@ -23,9 +23,11 @@
 
 namespace corridormap {
 
-triangle_list build_distance_mesh(polygon obstacle, unsigned /*resolution_x*/, unsigned /*resolution_y*/, memory* /*output*/)
+triangle_list build_distance_mesh(polygon /*obstacle*/, unsigned /*resolution_x*/, unsigned /*resolution_y*/, memory* /*output*/)
 {
     triangle_list result;
+    result.ntris = 0;
+    result.vertices = 0;
     return result;
 }
 
