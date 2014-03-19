3fa256fffaa3702444f449e23accc6cd677501cb
Alexander Shafranov <shafranov@gmail.com>
2014-03-19 11:57:23 +0100

    render interface init.

---
+++ include/corridormap/render_interface.h
@@ -0,0 +1,14 @@
+#ifndef CORRIDORMAP_RENDER_INTERFACE_H_
+#define CORRIDORMAP_RENDER_INTERFACE_H_
+
+namespace corridormap {
+
+class render_interface
+{
+public:
+    virtual ~render_interface() {}
+};
+
+}
+
+#endif
