56ebe66647dfc0f1de6db9d836cf743e61fda4a2
Alexander Shafranov <shafranov@gmail.com>
2014-03-24 22:39:12 +0100

    voronoi: rendring to frame buffer.

--- example/voronoi.cpp
+++ example/voronoi.cpp
@@ -1,27 +1,78 @@
+#include <stdio.h>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include "corridormap/render_gl.h"
 
+struct glfw_context
+{
+    bool ok;
+
+    glfw_context()
+    {
+        ok = !!glfwInit();
+    }
+
+    ~glfw_context()
+    {
+        glfwTerminate();
+    }
+};
+
+struct glfw_window_context
+{
+    GLFWwindow* window;
+
+    glfw_window_context(int width, int height, const char* title)
+    {
+        window = glfwCreateWindow(width, height, title, 0, 0);
+    }
+
+    ~glfw_window_context()
+    {
+        glfwDestroyWindow(window);
+    }
+};
+
+namespace
+{
+    const int screen_width = 1280;
+    const int screen_height = 720;
+}
+
 int main()
 {
-    if (!glfwInit())
+    glfw_context glfw_ctx;
+
+    if (!glfw_ctx.ok)
     {
+        fprintf(stderr, "failed to initialize GLFW context.\n");
         return 1;
     }
 
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "Voronoi", NULL, NULL);
+    glfw_window_context glfw_window_ctx(screen_width, screen_height, "Voronoi");
+    GLFWwindow* window = glfw_window_ctx.window;
 
     if (!window)
     {
-        glfwTerminate();
+        fprintf(stderr, "failed to create GLFW window.\n");
         return 1;
     }
 
     glfwMakeContextCurrent(window);
+
     glewInit();
 
     corridormap::renderer_gl render_iface;
-    render_iface.initialize(corridormap::renderer::parameters());
+    corridormap::renderer::parameters render_params;
+    render_params.render_target_width = screen_width;
+    render_params.render_target_height = screen_height;
+
+    if (!render_iface.initialize(render_params))
+    {
+        fprintf(stderr, "failed to initialize render interface.\n");
+        render_iface.finalize();
+        return 1;
+    }
 
     while (!glfwWindowShouldClose(window))
     {
@@ -33,14 +84,17 @@ int main()
 
         glViewport(0, 0, width, height);
         glClearColor(1.f, 0.f, 1.f, 1.f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        render_iface.begin();
+        render_iface.end();
+        render_iface.blit_frame_buffer(width, height);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    glfwDestroyWindow(window);
-    glfwTerminate();
+    render_iface.finalize();
 
     return 0;
 }
--- include/corridormap/render_gl.h
+++ include/corridormap/render_gl.h
@@ -72,42 +72,99 @@ namespace
 class renderer_gl : public renderer
 {
 public:
-    virtual bool initialize(renderer::parameters /*params*/)
+
+    virtual bool initialize(renderer::parameters params)
     {
+        _params = params;
+
+        glGenFramebuffers(1, &_frame_buffer);
+        glGenTextures(2, _output_textures);
+
+        // initialize output textures
+        {
+            glBindTexture(GL_TEXTURE_2D, _output_textures[0]);
+            glTexStorage2D(GL_TEXTURE_2D, 1, GL_RGBA8, params.render_target_width, params.render_target_height);
+
+            if (glGetError() != GL_NO_ERROR)
+            {
+                return false;
+            }
+
+            glBindTexture(GL_TEXTURE_2D, _output_textures[1]);
+            glTexStorage2D(GL_TEXTURE_2D, 1, GL_DEPTH_COMPONENT32F, params.render_target_width, params.render_target_height);
+
+            if (glGetError() != GL_NO_ERROR)
+            {
+                return false;
+            }
+
+            glBindTexture(GL_TEXTURE_2D, 0);
+        }
+
+        // attach textures to the frame buffer object.
+        {
+            glBindFramebuffer(GL_DRAW_FRAMEBUFFER, _frame_buffer);
+
+            glFramebufferTexture2D(GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, _output_textures[0], 0);
+
+            if (glGetError() != GL_NO_ERROR)
+            {
+                return false;
+            }
+
+            glFramebufferTexture2D(GL_DRAW_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, _output_textures[1], 0);
+
+            if (glGetError() != GL_NO_ERROR)
+            {
+                return false;
+            }
+
+            if (glCheckFramebufferStatus(GL_DRAW_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+            {
+                return false;
+            }
+
+            glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+        }
+
         glGenVertexArrays(1, &_vertex_array);
         glGenBuffers(1, &_vertex_buffer);
 
-        _program = glCreateProgram();
-        _vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-        _fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-
-        glShaderSource(_vertex_shader, 1, &vertex_shader, 0);
-        glShaderSource(_fragment_shader, 1, &fragment_shader, 0);
-
-        GLint status;
-        glCompileShader(_vertex_shader);
-        glCompileShader(_fragment_shader);
-
-        glGetShaderiv(_vertex_shader, GL_COMPILE_STATUS, &status);
-
-        if (status != GL_TRUE)
+        // initialize shaders and shader attributes.
         {
-            print_shader_log(_vertex_shader);
-            return false;
+            _program = glCreateProgram();
+            _vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+            _fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+
+            glShaderSource(_vertex_shader, 1, &vertex_shader, 0);
+            glShaderSource(_fragment_shader, 1, &fragment_shader, 0);
+
+            GLint status;
+            glCompileShader(_vertex_shader);
+            glCompileShader(_fragment_shader);
+
+            glGetShaderiv(_vertex_shader, GL_COMPILE_STATUS, &status);
+
+            if (status != GL_TRUE)
+            {
+                print_shader_log(_vertex_shader);
+                return false;
+            }
+
+            glGetShaderiv(_fragment_shader, GL_COMPILE_STATUS, &status);
+
+            if (status != GL_TRUE)
+            {
+                print_shader_log(_fragment_shader);
+                return false;
+            }
+
+            glAttachShader(_program, _vertex_shader);
+            glAttachShader(_program, _fragment_shader);
+
+            glBindAttribLocation(_program, 0, "position");
+            _wvp_location = glGetUniformLocation(_vertex_shader, "wvp");
         }
-
-        glGetShaderiv(_fragment_shader, GL_COMPILE_STATUS, &status);
-
-        if (status != GL_TRUE)
-        {
-            print_shader_log(_fragment_shader);
-            return false;
-        }
-
-        glAttachShader(_program, _vertex_shader);
-        glAttachShader(_program, _fragment_shader);
-
-        glBindAttribLocation(_program, 0, "position");
 
         return true;
     }
@@ -116,10 +173,16 @@ public:
     {
         glDeleteBuffers(1, &_vertex_buffer);
         glDeleteVertexArrays(1, &_vertex_array);
+        glDeleteTextures(2, _output_textures);
+        glDeleteFramebuffers(1, &_frame_buffer);
     }
 
     virtual void begin()
     {
+        glBindFramebuffer(GL_DRAW_FRAMEBUFFER, _frame_buffer);
+        glViewport(0, 0, _params.render_target_width, _params.render_target_height);
+        glClearColor(1.f, 0.f, 0.f, 1.f);
+        glClear(GL_COLOR_BUFFER_BIT);
     }
 
     virtual void draw(const float* /*vertices*/, unsigned /*count*/, unsigned /*color*/, renderer::primitive_type /*pt*/)
@@ -128,18 +191,32 @@ public:
 
     virtual void end()
     {
+        glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
     }
 
     virtual void read_pixels(unsigned char* /*destination*/)
     {
     }
 
+    void blit_frame_buffer(int width, int height)
+    {
+        glBindFramebuffer(GL_READ_FRAMEBUFFER, _frame_buffer);
+        glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+        glBlitFramebuffer(0, 0, _params.render_target_width, _params.render_target_height, 0, 0, width, height, GL_COLOR_BUFFER_BIT, GL_LINEAR);
+        glBindFramebuffer(GL_READ_FRAMEBUFFER, 0);
+    }
+
 private:
+    renderer::parameters _params;
+
+    GLuint _frame_buffer;
+    GLuint _output_textures[2];
     GLuint _vertex_array;
     GLuint _vertex_buffer;
     GLuint _program;
     GLuint _vertex_shader;
     GLuint _fragment_shader;
+    GLint  _wvp_location;
 };
 
 }
